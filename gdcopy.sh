2e7a1b9b5b2874d4947e784156889f2972e44fe8
chiyi4488 <chiyi4869@gmail.com>
2021-01-24 16:15:46 +0800

    Update gdcopy.sh

--- gdcopy.sh
+++ gdcopy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # gdsrc is source, gdrive_mirror is destination
-rclone copy gdrive_mirror:Cotpear備用載點 gdrive_backup:Cotpear備用載點(Team) --transfers 48 -v --drive-server-side-across-configs --fast-list
+rclone copy gdrive_mirror:Cotpear備用載點 gdrive_backup:Cotpear備用載點 --transfers 48 -v --drive-server-side-across-configs --fast-list
