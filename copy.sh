fed298f5db23f41dbfd3106d987c97a102b89454
chiyi4488 <chiyi4869@gmail.com>
2021-01-25 14:17:52 +0800

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-rclone copy gdrive_mirror: onedrive:"Cotpear 備用載點 (3, OneDrive)" --transfers 48 -q -i
-rclone copy onedrive:"Cotpear 備用載點 (3, OneDrive)" gdrive_mirror: --transfers 48 -q -i
+rclone copy gdrive_mirror: onedrive:"Cotpear 備用載點 (3, OneDrive)" --transfers 48 -v
+rclone copy onedrive:"Cotpear 備用載點 (3, OneDrive)" gdrive_mirror: --transfers 48 -v
